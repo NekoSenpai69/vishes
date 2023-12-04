623c65bac18665a3f1e553a44da225839fe86929
NekoSenpai69 <123672103+NekoSenpai69@users.noreply.github.com>
2023-12-04 20:42:41 +0000

    Initial commit

---
+++ data_types_2.js
@@ -0,0 +1,46 @@
+/** 
+java script:-
+  1) primitive data types:- st-stack
+    **a) number let a = 95; 
+    **b) string let b = '23'
+    *c) boolean let c = true;
+    *d) undefined let d = undefined
+    *e) null let e = null;
+    f) symbol let f = Symbol('hello');
+    let h = Symbol('hello');
+    g) bigint let g = BigInt('567');
+  2) non primitive data types:- st-heap
+    **a) object let obj = {
+    'name' : 'goku',
+    'age' : 23,
+    'address' : 'd ball z',
+    phone : 95846265,
+    book : {
+    name : 'harry potter',
+    page : 65
+    },
+  array : ['goku', 'vegeta', 'gohan']
+  
+    }
+    **b) array let array = [
+    'goku',
+    'gohan',
+    'luffy'
+    ]
+    
+    **c) function 
+    
+let obj = {
+  0 : 'goku',
+  1 : 'gohan',
+  2 : 'luffy'
+}
+
+let gojo = "567",sukuna = "789";
+
+let array = [
+  'goku',
+  'gohan',
+  'luffy'
+]
+*/
---
+++ index.js
@@ -0,0 +1,14 @@
+let GFGMap = new Map(); 
+
+// Adding a key value pair 
+GFGMap.set(1, "India"); 
+
+// Getting the value by key 
+console.log(GFGMap.get(1)); 
+
+// Updating the existing value 
+GFGMap.set(2, "England"); 
+
+// Getting the value back 
+console.log(GFGMap.get(2)); 
+console.log(GFGMap)
